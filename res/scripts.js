ec65d32c51895e79defb0284b9bba43cd27c0853
Joel Davenport <joelxdavenport@gmail.com>
2021-01-28 16:54:46 -0600

    css for sub nav

--- res/scripts.js
+++ res/scripts.js
@@ -1,13 +1,7 @@
-/*fetch("../res/header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-  });
-*/
 
 $(document).ready(function() {
+
     // import header and navigation
     $('header').load("../res/header.html");
+    
 });
