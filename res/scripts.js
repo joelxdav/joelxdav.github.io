005a9ae690fde30cb25419e4a777b61e98a1d3f7
Joel Davenport <joelxdavenport@gmail.com>
2021-02-04 13:09:17 -0600

    updating all index files and adding files

--- res/scripts.js
+++ res/scripts.js
@@ -2,6 +2,13 @@
 $(document).ready(function() {
 
     // import header and navigation
-    $('header').load("../res/header.html");
+    $('header').load("/res/header.html");
+
+    // import sidebar 
+    $('#sidebar').load("/res/sidebar.html");
+
+    // import footer
+    $('footer').load("/res/footer.html");
+
     
 });
