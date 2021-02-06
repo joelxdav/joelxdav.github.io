ca373faf8dbee5112184a6c59a74034fe1c0477f
Joel Davenport <joelxdavenport@gmail.com>
2021-02-05 22:26:31 -0600

    updating the index navigation

--- res/scripts.js
+++ res/scripts.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 
 
   // display "week" specific content in "content" div
-  function loadWeeklyContent() {
+  function loadWeeklyContent(week) {
     week_content = [
       "", // null
       '<li><a href="">1.3 - Create Your First HTML Page</a> <i>(the home page)</i></li><li><a href="https://github.com/joelxdav/joelxdav.github.io" target="_blank">1.4 - Files And Directories</a> <i>(github repository)</i></li><li><a href="in-the-beginning.html">1.5 - Using HTML To Structure Text</a></li>',
@@ -35,14 +35,26 @@ $(document).ready(function() {
     document.getElementById("assignments-list").innerHTML = week_content[week];
   }
 
-  // check to see if parameters are valid
+
+  // populate content area when sidebar buttons are clicked..."w" is for week number
+  $(".weeks").click(function(){
+    var w = this.name;
+    $('#content').html('<blockquote><h3>Assignments</h3><h5 id="week-h5"></h5><ul id="assignments-list"></ul><br><br></blockquote>');
+    loadWeeklyContent(w);
+  })
+
+
+
+  // check to see if parameters are valid, then populate content
   if (week > 0) {
     $('#content').html('<blockquote><h3>Assignments</h3><h5 id="week-h5"></h5><ul id="assignments-list"></ul><br><br></blockquote>');
-    loadWeeklyContent();
+    loadWeeklyContent(week);
   }
 
 
 
+// export functions 
+module.exports = { populateContent, loadWeeklyContent };
 
 /******************* NEW MODULE ****************************
 
@@ -50,10 +62,11 @@ $(document).ready(function() {
 
 */
 
-  $('header').load("/res/header.html");
-//$('#sidebar').load("/res/sidebar.html");
-  $('footer').load("/res/footer.html");
-
+  //$('header').load("/res/header.html");
+  //$('#sidebar').load("/res/sidebar.html");
+  //$('footer').load("/res/footer.html");
+ 
+  // now that the index is just one page, loading these from one source doesn't reduce any work
 
 
 });
